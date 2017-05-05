9cbcb21835e78b4fbae227c838a9aab15d120c69
Terry Duong <tduong10101@gmail.com>
2017-05-05 22:15:42 +1000

    Enable validation for 'all' validate on Hour,Minute and Time spinbox Tidy up all comments and validation functions
    Signed-off-by: Terry Duong <tduong10101@gmail.com>

--- main.py
+++ main.py
@@ -3,19 +3,14 @@ from time import *
 import Tkinter as tk
 import ttk
 duration = 0
-TEXT_MAXINPUTSIZE = 2
-'''
-def validateTextInputSize(event):
-    """ Method to Validate Entry text input size """
-    global TEXT_MAXINPUTSIZE
-    if (event.widget.index(tk.END) > TEXT_MAXINPUTSIZE - 1):
-        event.widget.delete(TEXT_MAXINPUTSIZE - 1)
+
 def CalDuration(strDate):
     targetTime = datetime.strptime(strDate, '%d/%m/%Y %H:%M')
     currentDate = datetime.now()
     duration = targetTime - currentDate
     return duration
-'''
+
+
 class YAlarm(tk.Frame):
     def __init__(self, parent):
         tk.Frame.__init__(self, parent)
@@ -36,43 +31,51 @@ class YAlarm(tk.Frame):
         self.hourText.set("0")
         minVal = 0
         maxVal = 12
-        vHourscmd = (self.register(self.validate), '%P', '%W', minVal, maxVal)
-        self.hourSB = tk.Spinbox(self, textvariable=self.hourText, from_=minVal, to=maxVal, width=3, validate="focusout",validatecommand=vHourscmd)
-
+        vHourscmd = (self.register(self.validate),'%d','%P','%W', minVal, maxVal)
+        self.hourSB = tk.Spinbox(self, textvariable=self.hourText, from_=minVal, to=maxVal, width=3, validate="all",validatecommand=vHourscmd)
         self.hourSB.grid(row=2, column=0, padx=20)
 
         minVal = 0
         maxVal = 60
-        vMinutescmd = (self.register(self.validate), '%P', '%W', minVal, maxVal)
         self.minuteText = tk.StringVar()
-        self.minuteSB = tk.Spinbox(self, textvariable=self.minuteText, from_=minVal, to=maxVal, width=3, validate='focusout',validatecommand=vMinutescmd)
+        vMinutescmd = (self.register(self.validate),'%d','%P', '%W', minVal, maxVal)
+        self.minuteSB = tk.Spinbox(self, textvariable=self.minuteText, from_=minVal, to=maxVal, width=3, validate='all',validatecommand=vMinutescmd)
         self.minuteSB.grid(row=2, column=1, padx=20)
-        vTimecmd = (self.register(self.validateTime),'%P', '%W')
-        self.timeSB = tk.Spinbox(self, value=("AM", "PM"), width=4, validate='focusout',validatecommand=vTimecmd)
+
+        vTimecmd = (self.register(self.validateTime),'%d','%S', '%W')
+        self.timeSB = tk.Spinbox(self, value=("AM", "PM"), width=4, validate='all',validatecommand=vTimecmd)
         self.timeSB.grid(row=2, column=2, padx=20)
 
-    def validate (self, P,W,minVal,maxVal):
+    def validate (self,d,P,W,minVal,maxVal):
         spinbox = self.nametowidget(W)
         try:
-            if int(P) < int(minVal):
-                spinbox.after_idle(lambda: spinbox.configure(value=minVal, validate='focusout'))
-                return False
-            elif int(P) > int(maxVal):
-                print maxVal
-                spinbox.after_idle(lambda: spinbox.configure(value=maxVal, validate='focusout'))
-                return False
+            if int(d)!= 0:
+                if not P:
+                    spinbox.after_idle(lambda: spinbox.configure(value=minVal,validate='all'))
+                    return False
+                elif int(P) > int(maxVal):
+                    return False
+                else:
+                    return True
             else:
                 return True
         except ValueError:
-            spinbox.after_idle(lambda: spinbox.configure(value=minVal, validate='focusout'))
+            spinbox.after_idle(lambda: spinbox.configure(value=minVal,validate='all'))
             return False
 
-    def validateTime (self, P,W):
-        if (P == "AM") or (P == "PM"):
-            return True
-        else:
-            self.nametowidget(W).after_idle(lambda: self.nametowidget(W).configure(value="AM", validate='focusout'))
+    def validateTime (self,d,S,W):
+        spinbox = self.nametowidget(W)
+        if int(d) == 1:
+            if S.lower() == "a":
+                value = ("AM", "PM")
+            elif S.lower() == "p":
+                value = ("PM", "AM")
+            else:
+                return False
+            spinbox.after_idle(lambda: spinbox.configure(value=value, validate='all'))
             return False
+        else:
+            return True
 
 
 def main():
@@ -83,21 +86,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-'''
-hourT = Entry (midFrame)
-hourT.bind("<Key>", validateTextInputSize)
-hourT["width"] = 2
-hourText = StringVar()
-hourText.set("0")
-vcmd = (midFrame.register(hourValidate),'%P')
-hourSB = Spinbox(midFrame,textvariable=hourText, from_=0, to=12,width=3,validate="focusout",validatecommand=vcmd)
-hourSB.grid(row=1,column=0,padx=20)
-
-minuteText = StringVar()
-minuteSB = Spinbox(midFrame,textvariable=minuteText, from_=0, to=60,width=3,validate='focusout')
-minuteSB.grid(row=1,column=1,padx=20)
-
-timeSB = Spinbox(midFrame, value=("AM","PM"),width=4)
-timeSB.grid(row=1,column=2,padx=20)
-'''
